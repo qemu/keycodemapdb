199ddce53eb5e1db1b9d4e3b3a38c551ed16388d
Pierre Ossman <ossman@cendio.se>
2017-05-31 15:51:59 +0200

    Handle string based codes
    Some systems use string identifiers rather than integers.
    
    Signed-off-by: Pierre Ossman <ossman@cendio.se>

--- tests/stdc++.cc
+++ tests/stdc++.cc
@@ -12,13 +12,42 @@
 
 #include "osx2win32.hh"
 #include "osx2win32_name.hh"
+
+#include "osx2xkb.hh"
+#include "osx2xkb_name.hh"
+
+#include "html2win32.hh"
+#include "html2win32_name.hh"
+
 #include "osx.hh"
 #include "osx_name.hh"
 
+#define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
+
 int main(int argc, char** argv)
 {
+	unsigned i;
+
 	assert(code_map_osx_to_win32[0x1d] == 0x30);
 	assert(strcmp(name_map_osx_to_win32[0x1d], "VK_0") == 0);
+
+	assert(strcmp(code_map_osx_to_xkb[0x1d], "AE10") == 0);
+	assert(strcmp(name_map_osx_to_xkb[0x1d], "AE10") == 0);
+
+	for (i = 0;i < ARRAY_SIZE(code_map_osx_to_win32);i++) {
+		if (strcmp(code_map_html_to_win32[i].from, "ControlLeft") == 0) {
+			assert(code_map_html_to_win32[i].to == 0x11);
+			break;
+		}
+	}
+	assert(i != ARRAY_SIZE(code_map_osx_to_win32));
+	for (i = 0;i < ARRAY_SIZE(name_map_html_to_win32);i++) {
+		if (strcmp(name_map_html_to_win32[i].from, "ControlLeft") == 0) {
+			assert(strcmp(name_map_html_to_win32[i].to, "VK_CONTROL") == 0);
+			break;
+		}
+	}
+	assert(i != ARRAY_SIZE(name_map_html_to_win32));
 
 	assert(code_table_osx[0x1d] == 0x3b);
 	assert(strcmp(name_table_osx[0x1d], "Control") == 0);
--- tests/stdc.c
+++ tests/stdc.c
@@ -12,13 +12,42 @@
 
 #include "osx2win32.h"
 #include "osx2win32_name.h"
+
+#include "osx2xkb.h"
+#include "osx2xkb_name.h"
+
+#include "html2win32.h"
+#include "html2win32_name.h"
+
 #include "osx.h"
 #include "osx_name.h"
 
+#define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
+
 int main(int argc, char** argv)
 {
+	unsigned i;
+
 	assert(code_map_osx_to_win32[0x1d] == 0x30);
 	assert(strcmp(name_map_osx_to_win32[0x1d], "VK_0") == 0);
+
+	assert(strcmp(code_map_osx_to_xkb[0x1d], "AE10") == 0);
+	assert(strcmp(name_map_osx_to_xkb[0x1d], "AE10") == 0);
+
+	for (i = 0;i < ARRAY_SIZE(code_map_osx_to_win32);i++) {
+		if (strcmp(code_map_html_to_win32[i].from, "ControlLeft") == 0) {
+			assert(code_map_html_to_win32[i].to == 0x11);
+			break;
+		}
+	}
+	assert(i != ARRAY_SIZE(code_map_osx_to_win32));
+	for (i = 0;i < ARRAY_SIZE(name_map_html_to_win32);i++) {
+		if (strcmp(name_map_html_to_win32[i].from, "ControlLeft") == 0) {
+			assert(strcmp(name_map_html_to_win32[i].to, "VK_CONTROL") == 0);
+			break;
+		}
+	}
+	assert(i != ARRAY_SIZE(name_map_html_to_win32));
 
 	assert(code_table_osx[0x1d] == 0x3b);
 	assert(strcmp(name_table_osx[0x1d], "Control") == 0);
