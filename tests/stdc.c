204930bad0adc366c0269e055559bb7882570a0e
Pierre Ossman <ossman@cendio.se>
2017-07-13 16:04:41 +0200

    Provide array size constant for C output
    The array might not be in the same unit as the code using it, so
    we need a way of indicating the number of entries.
    
    Signed-off-by: Pierre Ossman <ossman@cendio.se>

--- tests/stdc.c
+++ tests/stdc.c
@@ -28,28 +28,36 @@ int main(int argc, char** argv)
 {
 	unsigned i;
 
+	assert(code_map_osx_to_win32_len == ARRAY_SIZE(code_map_osx_to_win32));
 	assert(code_map_osx_to_win32[0x1d] == 0x30);
+	assert(name_map_osx_to_win32_len == ARRAY_SIZE(name_map_osx_to_win32));
 	assert(strcmp(name_map_osx_to_win32[0x1d], "VK_0") == 0);
 
+	assert(code_map_osx_to_xkb_len == ARRAY_SIZE(code_map_osx_to_xkb));
 	assert(strcmp(code_map_osx_to_xkb[0x1d], "AE10") == 0);
+	assert(name_map_osx_to_xkb_len == ARRAY_SIZE(name_map_osx_to_xkb));
 	assert(strcmp(name_map_osx_to_xkb[0x1d], "AE10") == 0);
 
-	for (i = 0;i < ARRAY_SIZE(code_map_osx_to_win32);i++) {
+	assert(code_map_html_to_win32_len == ARRAY_SIZE(code_map_html_to_win32));
+	for (i = 0;i < code_map_html_to_win32_len;i++) {
 		if (strcmp(code_map_html_to_win32[i].from, "ControlLeft") == 0) {
 			assert(code_map_html_to_win32[i].to == 0x11);
 			break;
 		}
 	}
-	assert(i != ARRAY_SIZE(code_map_osx_to_win32));
-	for (i = 0;i < ARRAY_SIZE(name_map_html_to_win32);i++) {
+	assert(i != code_map_html_to_win32_len);
+	assert(name_map_html_to_win32_len == ARRAY_SIZE(name_map_html_to_win32));
+	for (i = 0;i < name_map_html_to_win32_len;i++) {
 		if (strcmp(name_map_html_to_win32[i].from, "ControlLeft") == 0) {
 			assert(strcmp(name_map_html_to_win32[i].to, "VK_CONTROL") == 0);
 			break;
 		}
 	}
-	assert(i != ARRAY_SIZE(name_map_html_to_win32));
+	assert(i != name_map_html_to_win32_len);
 
+	assert(code_table_osx_len == ARRAY_SIZE(code_table_osx));
 	assert(code_table_osx[0x1d] == 0x3b);
+	assert(name_table_osx_len == ARRAY_SIZE(name_table_osx));
 	assert(strcmp(name_table_osx[0x1d], "Control") == 0);
 
 	return 0;
